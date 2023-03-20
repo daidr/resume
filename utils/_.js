ecad1e2f6ab26df3be271ca44d7d9f0082c1cffe
daidr <daidr@daidr.me>
2023-03-21 02:06:31 +0800

    feat: util functions

---
+++ utils/_.js
@@ -0,0 +1,24 @@
+export const debounce = (fn, delay) => {
+  let timer = null;
+  return function () {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, delay);
+  };
+};
+
+export const emptyDir = async (path) => {
+  const files = await fs.readdir(path);
+  for (let file of files) {
+    const filePath = join(path, file);
+    const stats = await fs.stat(filePath);
+    if (stats.isDirectory()) {
+      await emptyDir(filePath);
+    } else {
+      await fs.unlink(filePath);
+    }
+  }
+};
